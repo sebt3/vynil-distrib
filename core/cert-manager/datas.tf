ef2d001e544c05f4898d36bd07b4fbaa1e92eaab
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-06 14:20:32 +0200

    Added the initial packages

---
+++ core/cert-manager/datas.tf
@@ -0,0 +1,27 @@
+
+data "kustomization_overlay" "data" {
+  namespace = var.namespace
+  resources = [
+    "https://github.com/cert-manager/cert-manager/releases/download/v${var.release}/cert-manager.yaml",
+  ]
+  images {
+    name = "quay.io/jetstack/cert-manager-cainjector"
+    new_name = "${var.images.cainjector.registry}/${var.images.cainjector.repository}"
+    new_tag = "${var.images.cainjector.tag}"
+  }
+  images {
+    name = "quay.io/jetstack/cert-manager-controller"
+    new_name = "${var.images.controller.registry}/${var.images.controller.repository}"
+    new_tag = "${var.images.controller.tag}"
+  }
+  images {
+    name = "quay.io/jetstack/cert-manager-webhook"
+    new_name = "${var.images.webhook.registry}/${var.images.webhook.repository}"
+    new_tag = "${var.images.webhook.tag}"
+  }
+  images {
+    name = "quay.io/jetstack/cert-manager-ctl"
+    new_name = "${var.images.startupapicheck.registry}/${var.images.startupapicheck.repository}"
+    new_tag = "${var.images.startupapicheck.tag}"
+  }
+}
