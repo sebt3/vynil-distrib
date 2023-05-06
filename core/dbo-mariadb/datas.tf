26985735cff6c7a22c250cf69c6fcb1b4be422c3
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-06 16:24:14 +0200

    fix

--- core/dbo-mariadb/datas.tf
+++ core/dbo-mariadb/datas.tf
@@ -4,4 +4,21 @@ data "kustomization_overlay" "data" {
   resources = concat([
     "https://raw.githubusercontent.com/mariadb-operator/mariadb-operator/v0.0.11/deploy/crds/crds.yaml"
   ],[ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"])
+
+  patches {
+    target {
+      kind = "Certificate"
+      name = "mariadb-operator-webhook-cert"
+    }
+    patch = <<-EOF
+      apiVersion: cert-manager.io/v1
+      kind: Certificate
+      metadata:
+        name: mariadb-operator-webhook-cert
+      spec:
+        dnsNames:
+        - mariadb-operator-webhook.${var.namespace}.svc
+        - mariadb-operator-webhook.${var.namespace}.svc.cluster.local
+    EOF
+  }
 }
