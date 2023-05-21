7b422c21d9fcc307471226adbabe243979e937e1
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-21 20:53:49 +0200

    Adding traefik

---
+++ core/traefik/datas.tf
@@ -0,0 +1,24 @@
+
+data "kustomization_overlay" "data" {
+  namespace = var.namespace
+  resources = [ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"]
+  patches {
+    target {
+      kind = "DaemonSet"
+      name = "traefik"
+    }
+    patch = <<-EOF
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: traefik
+spec:
+  template:
+    spec:
+      containers:
+      - name: traefik
+        image: "${var.image.registry}/${var.image.repository}:${var.image.tag}"
+        imagePullPolicy: "${var.image.pullPolicy}"
+    EOF
+  }
+}
