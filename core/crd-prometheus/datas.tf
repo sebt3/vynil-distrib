591210438325f1270db0563aa97722ae6de9e684
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-21 16:21:26 +0200

    fix

--- core/crd-prometheus/datas.tf
+++ core/crd-prometheus/datas.tf
@@ -1,14 +1,8 @@
 
 data "kustomization_overlay" "data" {
   resources = [
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml",
     "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml",
     "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml",
+    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml"
   ]
 }
