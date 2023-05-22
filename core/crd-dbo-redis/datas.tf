62c2ec15953d5e48e9da607cef8fb8d44ae2358d
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-22 19:26:10 +0200

    Improve CRDs strategy

--- core/crd-dbo-redis/datas.tf
+++
@@ -1,4 +0,0 @@
-
-data "kustomization_overlay" "data" {
-  resources = [ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"]
-}
--- core/crd-prometheus/datas.tf
+++
@@ -1,7 +0,0 @@
-
-data "kustomization_overlay" "data" {
-  resources = [
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml",
-    "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${var.release}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml"
-  ]
-}
