bb7e4762dc84c99dc2469ac164a816cdc402e11f
Sébastien Huss <sebastien.huss@gmail.com>
2023-05-23 06:49:21 +0200

    Fixes

--- core/dbo-redis/datas.tf
+++ core/dbo-redis/datas.tf
@@ -1,12 +1,6 @@
 
 data "kustomization_overlay" "data" {
   namespace = var.namespace
-  crds = [
-    "https://raw.githubusercontent.com/OT-CONTAINER-KIT/helm-charts/redis-operator-${var.release}/charts/redis-operator/crds/redis-cluster.yaml",
-    "https://raw.githubusercontent.com/OT-CONTAINER-KIT/helm-charts/redis-operator-${var.release}/charts/redis-operator/crds/redis-replication.yaml",
-    "https://raw.githubusercontent.com/OT-CONTAINER-KIT/helm-charts/redis-operator-${var.release}/charts/redis-operator/crds/redis-sentinel.yaml",
-    "https://raw.githubusercontent.com/OT-CONTAINER-KIT/helm-charts/redis-operator-${var.release}/charts/redis-operator/crds/redis.yaml"
-  ]
   resources = [ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"]
   patches {
     target {
