0743d314c077264d48e23ee4a05dd152762b8b5f
Sébastien Huss <sebastien.huss@gmail.com>
2023-06-06 14:48:18 +0200

    Update cert manager

--- core/cert-manager/datas.tf
+++ core/cert-manager/datas.tf
@@ -23,4 +23,82 @@ data "kustomization_overlay" "data" {
     new_name = "${var.images.startupapicheck.registry}/${var.images.startupapicheck.repository}"
     new_tag = "${var.images.startupapicheck.tag}"
   }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "clusterissuers.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: clusterissuers.cert-manager.io
+    EOF
+  }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "challenges.acme.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: challenges.acme.cert-manager.io
+    EOF
+  }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "certificaterequests.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: certificaterequests.cert-manager.io
+    EOF
+  }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "issuers.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: issuers.cert-manager.io
+    EOF
+  }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "certificates.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: certificates.cert-manager.io
+    EOF
+  }
+  patches {
+    target {
+      kind = "CustomResourceDefinition"
+      name = "orders.acme.cert-manager.io"
+    }
+    patch = <<-EOF
+  $patch: delete
+  apiVersion: apiextensions.k8s.io/v1
+  kind: CustomResourceDefinition
+  metadata:
+    name: orders.acme.cert-manager.io
+    EOF
+  }
 }
