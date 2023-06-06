12a2643c4ed79980bef1787cf7ee57b52030086c
Sébastien Huss <sebastien.huss@gmail.com>
2023-06-06 15:13:54 +0200

    rabbitmq fix

--- core/dbo-rabbitmq/datas.tf
+++ core/dbo-rabbitmq/datas.tf
@@ -1,8 +1,6 @@
 data "kustomization_overlay" "data" {
   namespace = var.namespace
-  resources = [
-    "https://github.com/rabbitmq/cluster-operator/releases/v${var.release}/download/cluster-operator.yml",
-  ]
+  resources = [ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"]
   patches {
     target {
       kind = "Deployment"
@@ -18,32 +16,6 @@ spec:
       containers:
       - name: operator
         image: "${var.image.registry}/${var.image.repository}:${var.image.tag}"
-    EOF
-  }
-  patches {
-    target {
-      kind = "CustomResourceDefinition"
-      name = "rabbitmqclusters.rabbitmq.com"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: rabbitmqclusters.rabbitmq.com
-    EOF
-  }
-  patches {
-    target {
-      kind = "Namespace"
-      name = "rabbitmq-system"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: v1
-  kind: Namespace
-  metadata:
-    name: rabbitmq-system
     EOF
   }
 }
