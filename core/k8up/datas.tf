bb3c7fb3360614ef5c09e27777c94f19380dc42e
Sébastien Huss <sebastien.huss@gmail.com>
2023-06-12 09:09:32 +0200

    fix

--- core/k8up/datas.tf
+++ core/k8up/datas.tf
@@ -1,6 +1,5 @@
 
 data "kustomization_overlay" "data" {
-  namespace = var.namespace
   resources = [ for file in fileset(path.module, "*.yaml"): file if file != "index.yaml"]
   images {
     name = "ghcr.io/k8up-io/k8up"
@@ -11,12 +10,14 @@ data "kustomization_overlay" "data" {
     target {
       kind = "Deployment"
       name = "k8up"
+      namespace = "k8up"
     }
     patch = <<-EOF
       apiVersion: apps/v1
       kind: Deployment
       metadata:
         name: k8up
+        namespace: "${var.namespace}"
       spec:
         template:
           spec:
@@ -35,6 +36,60 @@ data "kustomization_overlay" "data" {
                 requests:
                   cpu: "${var.resources.requests.cpu}"
                   memory: "${var.resources.requests.memory}"
+    EOF
+  }
+  patches {
+    target {
+      kind = "PrometheusRule"
+      name = "k8up-rule"
+      namespace = "k8up"
+    }
+    patch = <<-EOF
+      apiVersion: monitoring.coreos.com/v1
+      kind: PrometheusRule
+      metadata:
+        name: k8up-rule
+        namespace: "${var.namespace}"
+    EOF
+  }
+  patches {
+    target {
+      kind = "ServiceMonitor"
+      name = "k8up-monitor"
+      namespace = "k8up"
+    }
+    patch = <<-EOF
+      apiVersion: monitoring.coreos.com/v1
+      kind: ServiceMonitor
+      metadata:
+        name: k8up-monitor
+        namespace: "${var.namespace}"
+    EOF
+  }
+  patches {
+    target {
+      kind = "Service"
+      name = "k8up-metrics"
+    }
+    patch = <<-EOF
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: k8up-metrics
+        namespace: "${var.namespace}"
+    EOF
+  }
+  patches {
+    target {
+      kind = "ServiceAccount"
+      name = "k8up"
+    }
+    patch = <<-EOF
+      apiVersion: v1
+      kind: ServiceAccount
+      metadata:
+        name: k8up
+        namespace: "${var.namespace}"
     EOF
   }
 }
