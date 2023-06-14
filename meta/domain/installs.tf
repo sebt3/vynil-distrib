4a6be9e12a0e888273cec7fb5ed0145a09e9be13
Sébastien Huss <sebastien.huss@gmail.com>
2023-06-14 14:23:55 +0200

    fixes

--- meta/domain/installs.tf
+++ meta/domain/installs.tf
@@ -5,20 +5,62 @@ locals {
         "issuer"         = var.issuer
         "ingress-class"  = var.ingress-class
     }
+    annotations = {
+      "vynil.solidite.fr/meta" = "domain"
+      "vynil.solidite.fr/name" = var.namespace
+      "vynil.solidite.fr/domain" = var.domain-name
+      "vynil.solidite.fr/issuer" = var.issuer
+    }
+    auth = { for k, v in var.auth : k => v if k!="enable" }
+    infra = { for k, v in var.infra : k => v if k!="enable" }
+    ci = { for k, v in var.ci : k => v if k!="enable" }
 }
-resource "kubectl_manifest" "installs" {
-  count = length(var.metas)
+
+resource "kubectl_manifest" "auth" {
+  count = var.auth.enable ? 1 : 0
   yaml_body  = <<-EOF
     apiVersion: "vynil.solidite.fr/v1"
     kind: "Install"
     metadata:
-      name: "${try(var.metas[count.index].component, "auth")}"
+      name: "auth"
       namespace: "${var.namespace}"
       labels: ${jsonencode(local.common-labels)}
     spec:
-      distrib: "${try(var.metas[count.index].distrib, var.default-dist)}"
+      distrib: "core"
       category: "meta"
-      component: "domain-${try(var.metas[count.index].component, "auth")}"
-      options: ${jsonencode(merge(local.global, try(var.metas[count.index].options, {})))}
+      component: "domain-auth"
+      options: ${jsonencode(merge(local.global, local.auth))}
+  EOF
+}
+resource "kubectl_manifest" "infra" {
+  count = var.infra.enable ? 1 : 0
+  yaml_body  = <<-EOF
+    apiVersion: "vynil.solidite.fr/v1"
+    kind: "Install"
+    metadata:
+      name: "infra"
+      namespace: "${var.namespace}"
+      labels: ${jsonencode(local.common-labels)}
+    spec:
+      distrib: "core"
+      category: "meta"
+      component: "domain-infra"
+      options: ${jsonencode(merge(local.global, local.infra))}
+  EOF
+}
+resource "kubectl_manifest" "ci" {
+  count = var.ci.enable ? 1 : 0
+  yaml_body  = <<-EOF
+    apiVersion: "vynil.solidite.fr/v1"
+    kind: "Install"
+    metadata:
+      name: "ci"
+      namespace: "${var.namespace}"
+      labels: ${jsonencode(local.common-labels)}
+    spec:
+      distrib: "core"
+      category: "meta"
+      component: "domain-ci"
+      options: ${jsonencode(merge(local.global, local.ci))}
   EOF
 }
