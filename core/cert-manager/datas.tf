e922c9c1c4d7aee4f0ca399e518c42609cf02cac
Sébastien Huss <sebastien.huss@gmail.com>
2023-06-29 11:32:02 +0200

    move cert-manager where it belong

--- core/cert-manager/datas.tf
+++ core/cert-manager/datas.tf
@@ -1,8 +1,25 @@
 
+locals {
+  common-labels = {
+    "vynil.solidite.fr/owner-name" = var.instance
+    "vynil.solidite.fr/owner-namespace" = var.namespace
+    "vynil.solidite.fr/owner-category" = var.category
+    "vynil.solidite.fr/owner-component" = var.component
+    "app.kubernetes.io/managed-by" = "vynil"
+    "app.kubernetes.io/name" = var.component
+    "app.kubernetes.io/instance" = var.instance
+  }
+  rb-patch = <<-EOF
+    - op: replace
+      path: /subjects/0/namespace
+      value: "${var.namespace}"
+    EOF
+
+}
 data "kustomization_overlay" "data" {
-  resources = [
-    "https://github.com/cert-manager/cert-manager/releases/download/v${var.release}/cert-manager.yaml",
-  ]
+  common_labels = local.common-labels
+  namespace = var.namespace
+  resources = [for file in fileset(path.module, "*.yaml"): file if file != "index.yaml" && length(regexall("ClusterRole",file))<1 && length(regexall("WebhookConfiguration",file))<1]
   images {
     name = "quay.io/jetstack/cert-manager-cainjector"
     new_name = "${var.images.cainjector.registry}/${var.images.cainjector.repository}"
@@ -23,82 +40,107 @@ data "kustomization_overlay" "data" {
     new_name = "${var.images.startupapicheck.registry}/${var.images.startupapicheck.repository}"
     new_tag = "${var.images.startupapicheck.tag}"
   }
+}
+
+data "kustomization_overlay" "data_no_ns" {
+  common_labels = local.common-labels
+  resources = [for file in fileset(path.module, "*.yaml"): file if file != "index.yaml" && (length(regexall("ClusterRole",file))>0 || length(regexall("WebhookConfiguration",file))>0)]
   patches {
     target {
-      kind = "CustomResourceDefinition"
-      name = "clusterissuers.cert-manager.io"
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-cainjector"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-approve:cert-manager-io"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-certificates"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-certificatesigningrequests"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-challenges"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-clusterissuers"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-ingress-shim"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-issuers"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-controller-orders"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "cert-manager-webhook:subjectaccessreviews"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "MutatingWebhookConfiguration"
+      name = "cert-manager-webhook"
     }
     patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: clusterissuers.cert-manager.io
+    - op: replace
+      path: /webhooks/0/clientConfig/service/namespace
+      value: "${var.namespace}"
+    - op: replace
+      path: /metadata/annotations/manager.io~1inject-ca-from-secret
+      value: "${var.namespace}/cert-manager-webhook-ca"
     EOF
   }
   patches {
     target {
-      kind = "CustomResourceDefinition"
-      name = "challenges.acme.cert-manager.io"
+      kind = "ValidatingWebhookConfiguration"
+      name = "cert-manager-webhook"
     }
     patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: challenges.acme.cert-manager.io
-    EOF
-  }
-  patches {
-    target {
-      kind = "CustomResourceDefinition"
-      name = "certificaterequests.cert-manager.io"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: certificaterequests.cert-manager.io
-    EOF
-  }
-  patches {
-    target {
-      kind = "CustomResourceDefinition"
-      name = "issuers.cert-manager.io"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: issuers.cert-manager.io
-    EOF
-  }
-  patches {
-    target {
-      kind = "CustomResourceDefinition"
-      name = "certificates.cert-manager.io"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: certificates.cert-manager.io
-    EOF
-  }
-  patches {
-    target {
-      kind = "CustomResourceDefinition"
-      name = "orders.acme.cert-manager.io"
-    }
-    patch = <<-EOF
-  $patch: delete
-  apiVersion: apiextensions.k8s.io/v1
-  kind: CustomResourceDefinition
-  metadata:
-    name: orders.acme.cert-manager.io
+    - op: replace
+      path: /webhooks/0/clientConfig/service/namespace
+      value: "${var.namespace}"
+    - op: replace
+      path: /metadata/annotations/manager.io~1inject-ca-from-secret
+      value: "${var.namespace}/cert-manager-webhook-ca"
     EOF
   }
 }
