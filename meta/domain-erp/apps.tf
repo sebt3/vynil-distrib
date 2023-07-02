57611a307449605922ecb488e9ee869a71be3722
Sébastien Huss <sebastien.huss@gmail.com>
2023-07-02 23:26:54 +0200

    fixes

--- meta/domain-erp/apps.tf
+++ meta/domain-erp/apps.tf
@@ -32,7 +32,7 @@ resource "kubectl_manifest" "dolibarr" {
     kind: "Install"
     metadata:
       name: "dolibarr"
-      namespace: "${kubernetes_namespace_v1.erp-ns[0].name}"
+      namespace: "${kubernetes_namespace_v1.erp-ns[0].metadata.name}"
       labels: ${jsonencode(local.common-labels)}
     spec:
       distrib: "core"
