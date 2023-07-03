337c148beda656669c5411d18b4378c914e3f8e7
Sébastien Huss <sebastien.huss@gmail.com>
2023-07-03 12:30:47 +0200

    fixes

--- apps/dolibarr/svc.tf
+++ apps/dolibarr/svc.tf
@@ -5,14 +5,14 @@ kind: Service
 metadata:
   name: ${var.instance}
   namespace: "${var.namespace}"
-  labels: ${jsonencode(local.common-labels)}
+  labels: ${jsonencode(local.deploy-labels)}
 spec:
   ports:
   - name: http
     port: 80
     protocol: TCP
     targetPort: 3000
-  selector: ${jsonencode(local.common-labels)}
+  selector: ${jsonencode(local.deploy-labels)}
   type: ClusterIP
   EOF
 }
