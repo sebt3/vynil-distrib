c388ab370c15d690c154fdf444e2875f8c051576
Sébastien Huss <sebastien.huss@gmail.com>
2024-01-22 13:29:34 +0100

    fix

--- meta/core/tools.tf
+++ meta/core/tools.tf
@@ -1,10 +1,11 @@
 locals {
     mayfly = { for k, v in var.tools.mayfly : k => v if k!="enable" }
     reloader = { for k, v in var.tools.reloader : k => v if k!="enable" }
+    prometheus = { for k, v in var.tools.prometheus : k => v if k!="enable" }
 }
 
 resource "kubernetes_namespace_v1" "tools-ns" {
-  count = ( var.tools.reloader.enable || var.tools.mayfly.enable )? 1 : 0
+  count = ( var.tools.prometheus.enable || var.tools.reloader.enable || var.tools.mayfly.enable )? 1 : 0
   metadata {
     annotations = local.annotations
     labels = local.common-labels
@@ -45,5 +46,23 @@ resource "kubectl_manifest" "reloader" {
       category: "core"
       component: "reloader"
       options: ${jsonencode(local.reloader)}
+  EOF
+}
+
+resource "kubectl_manifest" "prometheus" {
+  count = var.tools.prometheus.enable ? 1 : 0
+  depends_on = [kubernetes_namespace_v1.tools-ns]
+  yaml_body  = <<-EOF
+    apiVersion: "vynil.solidite.fr/v1"
+    kind: "Install"
+    metadata:
+      name: "prometheus"
+      namespace: "${var.tools.namespace}"
+      labels: ${jsonencode(local.common-labels)}
+    spec:
+      distrib: "core"
+      category: "core"
+      component: "prometheus"
+      options: ${jsonencode(local.prometheus)}
   EOF
 }
