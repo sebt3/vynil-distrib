3f0bde47e78ca28e834e71c58f7f37f9eb4123e0
Sébastien Huss <sebastien.huss@gmail.com>
2024-02-22 15:29:02 +0100

    fix

--- core/metrics-server/datas.tf
+++ core/metrics-server/datas.tf
@@ -8,6 +8,11 @@ locals {
     "app.kubernetes.io/name" = var.component
     "app.kubernetes.io/instance" = var.instance
   }
+  rb-patch = <<-EOF
+  - op: replace
+    path: /subjects/0/namespace
+    value: "${var.namespace}"
+  EOF
 }
 
 data "kustomization_overlay" "data" {
@@ -19,4 +24,34 @@ data "kustomization_overlay" "data" {
 data "kustomization_overlay" "data_no_ns" {
   common_labels = local.common-labels
   resources = [for file in fileset(path.module, "*.yaml"): file if file != "index.yaml" && (length(regexall("APIService",file))>0 || length(regexall("ClusterRole",file))>0) ]
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "metrics-server:system:auth-delegator"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "ClusterRoleBinding"
+      name = "system:metrics-server"
+    }
+    patch = local.rb-patch
+  }
+  patches {
+    target {
+      kind = "APIService"
+      name = "v1beta1.metrics.k8s.io"
+    }
+    patch = <<-EOF
+      apiVersion: apiregistration.k8s.io/v1
+      kind: APIService
+      metadata:
+        name: v1beta1.metrics.k8s.io
+      spec:
+        service:
+          name: metrics-server
+          namespace: "${var.namespace}"
+    EOF
+  }
 }
