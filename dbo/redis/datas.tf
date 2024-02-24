367e42912abbe817f3c5f4d99c6eca9ae69569c4
Sébastien Huss <sebastien.huss@gmail.com>
2024-02-24 17:32:36 +0100

    fix

--- dbo/redis/datas.tf
+++ dbo/redis/datas.tf
@@ -47,122 +47,20 @@ data "kustomization_overlay" "data" {
                   memory: "${var.resources.requests.memory}"
     EOF
   }
-}
-
-data "kustomization_overlay" "data_no_ns" {
-  common_labels = local.common-labels
-  resources = [for file in fileset(path.module, "*.yaml"): file if file != "index.yaml" && (length(regexall("ClusterRole",file))>0 || length(regexall("WebhookConfiguration",file))>0)]
   patches {
     target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-cainjector"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-approve:cert-manager-io"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-certificates"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-certificatesigningrequests"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-challenges"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-clusterissuers"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-ingress-shim"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-issuers"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-controller-orders"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator-certmanager-webhook:subjectaccessreviews"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "ClusterRoleBinding"
-      name = "redis-operator"
-    }
-    patch = local.rb-patch
-  }
-  patches {
-    target {
-      kind = "MutatingWebhookConfiguration"
-      name = "prometheus-community-kube-admission"
+      kind = "Certificate"
+      name = "redis-cert"
     }
     patch = <<-EOF
-    - op: replace
-      path: /webhooks/0/clientConfig/service/namespace
-      value: "${var.namespace}"
-    - op: replace
-      path: /metadata/annotations/certmanager.k8s.io~1inject-ca-from
-      value: "${var.namespace}/prometheus-community-kube-admission"
-    - op: replace
-      path: /metadata/annotations/cert-manager.io~1inject-ca-from
-      value: "${var.namespace}/prometheus-community-kube-admission"
-    EOF
-  }
-  patches {
-    target {
-      kind = "ValidatingWebhookConfiguration"
-      name = "prometheus-community-kube-admission"
-    }
-    patch = <<-EOF
-    - op: replace
-      path: /webhooks/0/clientConfig/service/namespace
-      value: "${var.namespace}"
-    - op: replace
-      path: /metadata/annotations/certmanager.k8s.io~1inject-ca-from
-      value: "${var.namespace}/prometheus-community-kube-admission"
-    - op: replace
-      path: /metadata/annotations/cert-manager.io~1inject-ca-from
-      value: "${var.namespace}/prometheus-community-kube-admission"
+      apiVersion: cert-manager.io/v1
+      kind: Certificate
+      metadata:
+        name: redis-cert
+      spec:
+        dnsNames:
+          - webhook-service.${var.namespace}.svc
+          - webhook-service.${var.namespace}.svc.cluster.local
     EOF
   }
 }
-
-
