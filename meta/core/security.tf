35e4d9baab2f728e82060d2d5298670dcdabee72
Sébastien Huss <sebastien.huss@gmail.com>
2024-06-17 14:16:06 +0200

    adding hnc and kuberest

--- meta/core/security.tf
+++ meta/core/security.tf
@@ -1,7 +1,6 @@
 locals {
     hnc = { for k, v in var.security.hnc : k => v if k!="enable" }
     kyverno = { for k, v in var.security.kyverno : k => v if k!="enable" }
-    capsule = { for k, v in var.security.capsule : k => v if k!="enable" }
     secret-generator = { for k, v in var.security.secret-generator : k => v if k!="enable" }
     cert-manager = { for k, v in var.security.cert-manager : k => v if k!="enable" }
     letsencrypt = { for k, v in var.security.letsencrypt : k => v if k!="enable" }
@@ -15,24 +14,6 @@ resource "kubernetes_namespace_v1" "security-ns" {
     labels = local.common-labels
     name = var.security.namespace
   }
-}
-
-resource "kubectl_manifest" "capsule" {
-  count = var.security.capsule.enable ? 1 : 0
-  depends_on = [kubernetes_namespace_v1.security-ns]
-  yaml_body  = <<-EOF
-    apiVersion: "vynil.solidite.fr/v1"
-    kind: "Install"
-    metadata:
-      name: "capsule"
-      namespace: "${var.security.namespace}"
-      labels: ${jsonencode(local.common-labels)}
-    spec:
-      distrib: "${var.component}"
-      category: "core"
-      component: "capsule"
-      options: ${jsonencode(local.capsule)}
-  EOF
 }
 
 resource "kubectl_manifest" "kyverno" {
